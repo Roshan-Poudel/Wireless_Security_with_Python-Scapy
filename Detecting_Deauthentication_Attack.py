1208d5d77bb3a2a2ceabe285834f904b5c0b51bf
Roshan Poudel <rowshanpoudel@gmail.com>
2020-07-09 17:13:14 +0545

    Update Detecting_Deauthentication_Attack.py

--- Detecting_Deauthentication_Attack.py
+++ Detecting_Deauthentication_Attack.py
@@ -1,11 +1,14 @@
 from scapy.all import *
+# importing scapy for packet analyzing
 import time,datetime
+# importing datetime for timestamp
 def detect_deauth_attack(pkt):
+	# Deauth attack detecting function
 	if pkt.haslayer(Dot11Deauth):
+		# Analyzing if packet contain Deauth layer
 		time=datetime.datetime.today()
+		# Generating timestamp
 		a= ' [ ' +  str(time)+ ' ] '+  ' Deauthentication Attack Detected Against Mac Address: ' +   str(pkt.addr2).swapcase()    
 		print(a)
-		log_file=open('Deauth.log','a')
-		enter=a+'\n'
-		log_file.write(enter)
 sniff(prn=detect_deauth_attack,count=0)
+# Sniffing packet and sending them to deauth detect function
